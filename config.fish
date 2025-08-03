9ef55d8b22b0812624acfd2c33196874f090ce3c
Bandar <127664309+bmuraykhi@users.noreply.github.com>
2025-08-03 12:09:32 +0000

    first commit

---
+++ config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    eval (/opt/homebrew/bin/brew shellenv)
+    fish_add_path /opt/homebrew/bin $HOME/.local/bin
+
+    alias vim=nvim
+    alias mylab='ssh homelab'
+    alias rsync='rsync -vzP --no-perms --no-owner --no-group'
+    alias csync='rsync -vzP --no-perms --no-owner --no-group --bwlimit=1000 --compress-level=9'
+    alias ls='ls -rthG'
+    alias genpass='python3 ~/genpass.py'
+
+    set -g fish_history save
+end
